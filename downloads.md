7fcb0c9840eae0081e536224b8227ea76ab21445
Sebastien Coavoux <s.coavoux@free.fr>
2015-11-17 22:35:02 -0500

    Add build, download jinja template and try to regenerate download file

--- downloads.md
+++ downloads.md
@@ -5,5 +5,16 @@ permalink: /download/
 menu: true
 ---
 
-The best place where know how to download and install is in the [documentation](http://alignak-doc.readthedocs.org/en/latest/02_installation/index.html).
+If you want recent builds have a look here :
 
+
+* [debian_8](build/debian_8/alignak-all_0.1-1.1446150306.0cf348d_all.deb)
+
+* [centos_7](build/centos_7/alignak-all-0.1-1_1446150306_0cf348d.x86_64.rpm)
+
+* [ubuntu_14](build/ubuntu_14/alignak-all_0.1-1.1446150306.0cf348d_all.deb)
+
+* [ubuntu_12](build/ubuntu_12/alignak-all_0.1-1.1446150306.0cf348d_all.deb)
+
+
+If you want the repository link for last tagged version have a look into the [documentation](http://alignak-doc.readthedocs.org/en/latest/02_installation/index.html).
---
+++ gen_download.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python 
+
+
+import sys
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
+if len(sys.argv) > 1:
+    ALIGNAK_PATH = sys.argv[1]
+else:
+    ALIGNAK_PATH = os.getcwd()
+
+TEMPLATE = "downloads.template"
+
+env = Environment(loader=FileSystemLoader(ALIGNAK_PATH))
+template = env.get_template(TEMPLATE)
+
+distros = []
+for dirpath, dirnames, filenames in os.walk(os.path.join(ALIGNAK_PATH,"build")):
+    if len(filenames) > 0:
+        distros.append({"name": dirpath.split("/")[-1],
+                        "link": os.path.join('build', dirpath.split("/")[-1], filenames[0])})
+
+out_file = file(os.path.join(ALIGNAK_PATH, TEMPLATE.replace("template","md")),'w')
+out_file.write(template.render(distros=distros))
+out_file.close()
